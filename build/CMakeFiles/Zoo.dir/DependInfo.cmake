139e9249f05195bcc255a744337612af1e393d9f
Tim Mudarisov <mudarisov96@mail.ru>
2023-07-29 01:25:43 +0300

    Initial commit

---
+++ build/CMakeFiles/Zoo.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/animal.cpp" "CMakeFiles/Zoo.dir/src/animal.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/animal.cpp.o.d"
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/bullet.cpp" "CMakeFiles/Zoo.dir/src/bullet.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/bullet.cpp.o.d"
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/controller.cpp" "CMakeFiles/Zoo.dir/src/controller.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/controller.cpp.o.d"
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/game.cpp" "CMakeFiles/Zoo.dir/src/game.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/game.cpp.o.d"
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/main.cpp" "CMakeFiles/Zoo.dir/src/main.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/main.cpp.o.d"
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/objects.cpp" "CMakeFiles/Zoo.dir/src/objects.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/objects.cpp.o.d"
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/player.cpp" "CMakeFiles/Zoo.dir/src/player.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/player.cpp.o.d"
+  "/Users/timurmudarisov/C++ prep/Udacity/CppND-Capstone-Zoo-Escape/Zoo-Escape-Game/src/renderer.cpp" "CMakeFiles/Zoo.dir/src/renderer.cpp.o" "gcc" "CMakeFiles/Zoo.dir/src/renderer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Zoo.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zoo.dir/src/animal.cpp.o"
+  "CMakeFiles/Zoo.dir/src/animal.cpp.o.d"
+  "CMakeFiles/Zoo.dir/src/bullet.cpp.o"
+  "CMakeFiles/Zoo.dir/src/bullet.cpp.o.d"
+  "CMakeFiles/Zoo.dir/src/controller.cpp.o"
+  "CMakeFiles/Zoo.dir/src/controller.cpp.o.d"
+  "CMakeFiles/Zoo.dir/src/game.cpp.o"
+  "CMakeFiles/Zoo.dir/src/game.cpp.o.d"
+  "CMakeFiles/Zoo.dir/src/main.cpp.o"
+  "CMakeFiles/Zoo.dir/src/main.cpp.o.d"
+  "CMakeFiles/Zoo.dir/src/objects.cpp.o"
+  "CMakeFiles/Zoo.dir/src/objects.cpp.o.d"
+  "CMakeFiles/Zoo.dir/src/player.cpp.o"
+  "CMakeFiles/Zoo.dir/src/player.cpp.o.d"
+  "CMakeFiles/Zoo.dir/src/renderer.cpp.o"
+  "CMakeFiles/Zoo.dir/src/renderer.cpp.o.d"
+  "Zoo"
+  "Zoo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
